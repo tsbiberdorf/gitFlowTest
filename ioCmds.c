e5c02b63fc54dbc9e38f680aa0b91111ef656566
Terry Biberdorf <tbiberdorf@littelfuse.com>
2015-03-10 14:49:49 -0600

    added ioCmds.c file

---
+++ ioCmds.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int32_t ioCmd( char Command)
+{
+	switch(Command)
+	{
+	case 0:
+		printf("help");
+		break;
+	case 1:
+		printf("go away");
+		break;
+	default:
+		printf("oh no!!");
+	}
+}
